95bf5ae74a41f60e68aa5c737299daa1fe79f405
shawnd121 <142047263+shawnd121@users.noreply.github.com>
2024-12-11 21:57:45 -0500

    Delete website directory

--- website/listhomesecuritycategories.test.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('homesecuritycategory.php');
-$categories = HomeSecurityCategory::getCategories();
-foreach ($categories as $category) {
-    $categoryID = $category->categoryID;
-    $name = $categoryID . " - " . $category->categoryCode . ", " . $category->categoryName;
-    echo "$name<br>";
-}
-?>
